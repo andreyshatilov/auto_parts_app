65921983aa591bcb82c6ba0331d8931b38117aed
Eduminuta Developer <startup@eduminuta.pl>
2026-08-01 10:44:54 +0200

    chore: rewrite README, remove 50+ junk files from repo, update gitignore

--- check_dups.py
+++
@@ -1,7 +0,0 @@
-import io
-
-with io.open('client_frontend/index.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-
-print('mainScreen count:', html.count('id="mainScreen"'))
-print('authScreen count:', html.count('id="authScreen"'))
--- check_profile_ids.py
+++
@@ -1,9 +0,0 @@
-import io
-
-with io.open('client_frontend/index.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-
-ids = ['profileFullName', 'profilePhone', 'profileEmail', 'profileShipping']
-for i in ids:
-    if f'"{i}"' not in html and f"'{i}'" not in html:
-        print(f'{i} MISSING in HTML')
